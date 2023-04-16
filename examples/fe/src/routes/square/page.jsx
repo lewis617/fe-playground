e28dd67e0a73680f993685735b089ac14415c9c7
liuyiqi.cn <liuyiqi.cn@bytedance.com>
2023-04-16 16:13:06 +0800

    自适应正方形

---
+++ examples/fe/src/routes/square/page.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export default function page() {
+  return (
+    <div className="container">
+      <div className="square"></div>
+      <div className="square2"></div>
+    </div>
+  );
+}
